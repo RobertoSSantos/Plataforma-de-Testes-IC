0fe9766c54acbed60c9b5f0e542f1324886d8bd8
Roberto Santos <rssan03@gmail.com>
2023-08-25 17:52:14 -0300

    commit

--- plataformaTestes.py
+++ plataformaTestes.py
@@ -1,9 +1,81 @@
 import flet as ft
 import time
 
-def main (page: ft.Page):
+class Basetextfield(ft.UserControl):
+    def __init__(self, label,width):
+        super().__init__()
+        self.label = label
+        self.width = width
+    
+    def build(self):
+        return ft.TextField(
+            label=self.label,
+            width=self.width
+        )
+    
+class Radiobuttons(ft.UserControl):
+    def __init__ (self):
+        super().__init__()
+    
+    def build(self):
+        return ft.Column(
+            controls=[
+                ft.Text("Como você se identifica?"),
+                ft.RadioGroup(content=ft.Column([
+                ft.Radio(value="Homem cis", label="Homem cis"),
+                ft.Radio(value="Homem trans", label="Homem trans"),
+                ft.Radio(value="Mulher cis", label="Mulher cis"),
+                ft.Radio(value="Mulher trans", label="Mulher trans"),
+                ft.Radio(value="não-binário", label="não-binário")]))
+            ]
+        )
 
-    #Base references
+class Schoolingdropdown(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+    
+    def build(self):
+        return ft.Dropdown(
+            label="Escolaridade",
+            hint_text="Selecione seu nivel de escolaridade",
+            width=400,
+            options=[
+                ft.dropdown.Option("Ensino Fundamental"),
+                ft.dropdown.Option("Ensino Médio"),
+                ft.dropdown.Option("Ensino Superior"),
+                ft.dropdown.Option("Pós-graduação"),
+                ft.dropdown.Option("Mestrado"),
+                ft.dropdown.Option("Doutorado"),
+                ft.dropdown.Option("Pós-doutorado")
+            ],
+        )
+
+class Elementsdropdown(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+    
+    def build(self):
+        return ft.Dropdown(
+            label="Elementos de maquina",
+            hint_text="Qual seu nivel de conhecimento sobre elementos de maquina",
+            width=400,
+            options=[
+                ft.dropdown.Option("Nenhum"),
+                ft.dropdown.Option("Tenho pouco conhecimento"),
+                ft.dropdown.Option("Estou aprendendo atualmente"),
+                ft.dropdown.Option("Tenho um bom entendimento"),
+            ],
+        )
+
+def main (page: ft.Page):
+    page.title = "Menu - Plataforma de Testes"
+    page.theme_mode = "light"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.scroll= ft.ScrollMode.AUTO
+    page.splash = ft.ProgressBar(visible=False)
+    page.window_always_on_top = True
+    page.window_center()
 
     #Toggle Dark/Light Mode
     def changetheme(e):
@@ -14,57 +86,42 @@ def main (page: ft.Page):
         toggledarklight.selected = not toggledarklight.selected
         page.splash.visible = False
         page.update()
-
-
-
-    #TextFields
-    name_txt = ft.TextField(
-        label="Nome",
-    )
     
+    def submit(e):
+        pass
 
-    #Toggle Dark/Light Mode Button
-    toggledarklight = ft.IconButton(
-        on_click=changetheme,
-        icon="dark_mode",
-        selected_icon="light_mode",
-        style=ft.ButtonStyle(
-            color={"":ft.colors.BLACK, "selected":ft.colors.WHITE}
-        )
-    )
 
-    #Start Tests Button
-
-    # Page definitions
-    page.title = "Menu - Plataforma de Testes"
-    page.theme_mode = "light"
-    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.scroll= ft.ScrollMode.AUTO
-    page.splash = ft.ProgressBar(visible=False)
-    page.window_center()
-    page.window_always_on_top = True
-
-    # Appbar
-    page.appbar = ft.AppBar(
-        title= ft.Text("Plataforma de testes", size=30),
-        center_title=True,
-        bgcolor='blue',
-        leading=ft.Icon(name="home"), 
-        actions=[
-            toggledarklight
-        ]
-    )
-    page.update()
+    placeholderCountdown = ft.Text("00:00")
+    formtitle = ft.Text(value="Teste de Identidade",style=ft.TextThemeStyle.DISPLAY_SMALL)
+    agetextfield = Basetextfield("Informe sua idade", 200)
+    genderradiobuttons = Radiobuttons()
+    schoolingdropdown = Schoolingdropdown()
+    coursetextfield = Basetextfield("Qual curso você realiza atualmente", 300)
+    elementsdropdown = Elementsdropdown()
+    submitbutton = ft.FilledButton("Enviar")
 
     page.add(
+        ft.AppBar(
+            title= ft.Text("Plataforma de testes", size=30),
+            center_title=True,
+            bgcolor='blue',
+            leading=ft.Icon(name="home"),
+            actions=[
+                placeholderCountdown,
+                toggledarklight
+            ]
+        ),
         ft.Column(
             controls=[
-                ft.Text("Formulario Inicial"),
-
+                formtitle,
+                agetextfield,
+                genderradiobuttons,
+                schoolingdropdown,
+                coursetextfield,
+                elementsdropdown,
+                submitbutton
             ]
-        )
+        ),
     )
     
-
 ft.app(target=main)
